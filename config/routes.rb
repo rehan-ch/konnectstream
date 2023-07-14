05433c72ad25f25453d270afedfb2fbe5bb091ef
Muhammad Rehan <rehanch642@gmail.com>
2023-07-14 10:35:23 +0500

    feat: form design update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
+
+  resources :designs, only: [] do
+    collection do
+      get :registration
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
